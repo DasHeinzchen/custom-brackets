33289d12203d92409e950c88c917ea234a12ecc0
DasHeinzchen <marlon.heinz@outlook.de>
2025-12-23 13:39:07 +0100

    Match connection logic

--- src/bracket_engine/models.py
+++ src/bracket_engine/models.py
@@ -1,5 +1,11 @@
 from typing import Optional
+from enum import Enum, auto
 from .exceptions import BracketError
+
+
+class LinkType(Enum):
+    WINNER = auto()
+    LOSER = auto()
 
 
 class Match:
@@ -7,10 +13,10 @@ class Match:
         self._opponent1 = None
         self._opponent2 = None
         self._winning_side = 0
-        self._winnerTo: Optional['Match'] = None
-        self._loserTo: Optional['Match'] = None
-        self._opponent1From: Optional['Match'] = None
-        self._opponent2From: Optional['Match'] = None
+        self._winner_to: Optional['Match'] = None
+        self._loser_to: Optional['Match'] = None
+        self._opponent1_from: Optional['Match'] = None
+        self._opponent2_from: Optional['Match'] = None
         self._qualified_number = qualified_number
 
     @property
@@ -26,32 +32,61 @@ class Match:
         return self._winning_side
 
     @property
-    def winnerTo(self) -> Optional['Match']:
-        return self._winnerTo
+    def winner_to(self) -> Optional['Match']:
+        return self._winner_to
 
     @property
-    def loserTo(self) -> Optional['Match']:
-        return self._loserTo
+    def loser_to(self) -> Optional['Match']:
+        return self._loser_to
 
     @property
-    def opponent1From(self) -> Optional['Match']:
-        return self._opponent1From
+    def opponent1_from(self) -> Optional['Match']:
+        return self._opponent1_from
 
     @property
-    def opponent2From(self) -> Optional['Match']:
-        return self._opponent2From
+    def opponent2_from(self) -> Optional['Match']:
+        return self._opponent2_from
 
     @property
     def qualified_number(self) -> int:
         return self.qualified_number
 
     def recieve_opponent(self, opponent, source_match: 'Match'):
-        if source_match == self._opponent1From:
+        if source_match == self._opponent1_from:
             self._opponent1 = opponent
-        elif source_match == self._opponent2From:
+        elif source_match == self._opponent2_from:
             self._opponent2 = opponent
         else:
             raise BracketError("Source match is unknown as a provider for this match.")
+
+    def disconnect(self, target: 'Match'):
+        if self._winner_to == target:
+            self._winner_to = None
+        elif self._loser_to == target:
+            self._loser_to = None
+        else:
+            raise BracketError("Matches are not fully connected")
+
+        if target.opponent1_from == self:
+            target._opponent1_from = None
+        elif target.opponent2_from == self:
+            target._opponent2_from = None
+        else:
+            raise BracketError("Matches are not fully connected")
+
+    def connect_to(self, target: 'Match', as_type: LinkType, target_slot: int):
+        if target_slot not in (1, 2):
+            raise BracketError(f"Invalid target slot: {target_slot}. Must be 1 or 2.")
+
+        if as_type == LinkType.WINNER:
+            self._winner_to = target
+        else:
+            self._loser_to = target
+
+        if target_slot == 1:
+            target._opponent1_from = self
+        else:
+            target._opponent2_from = self
 
     def set_winner(self, winning_side: int):
         if winning_side not in (1, 2):
@@ -62,7 +97,7 @@ class Match:
         winner = self._opponent1 if winning_side == 1 else self._opponent2
         loser = self._opponent1 if winning_side == 2 else self._opponent2
 
-        if self._winnerTo:
-            self._winnerTo.recieve_opponent(winner, self)
-        if self._loserTo:
-            self._loserTo.recieve_opponent(loser, self)
+        if self._winner_to:
+            self._winner_to.recieve_opponent(winner, self)
+        if self._loser_to:
+            self._loser_to.recieve_opponent(loser, self)
--- tests/test_match.py
+++ tests/test_match.py
@@ -1,6 +1,46 @@
 import pytest
-from bracket_engine.models import Match
+from bracket_engine.models import Match, LinkType
 from bracket_engine.exceptions import BracketError
+
+
+def test_connection_logic():
+    m1 = Match()
+    m2 = Match()
+
+    m1.connect_to(m2, LinkType.WINNER, 1)
+    assert m1.winner_to == m2
+    assert m1.loser_to is None
+    assert m2.opponent1_from == m1
+    assert m2.opponent2_from is None
+
+    m1.disconnect(m2)
+    m1.connect_to(m2, LinkType.LOSER, 1)
+    assert m1.winner_to is None
+    assert m1.loser_to == m2
+    assert m2.opponent1_from == m1
+    assert m2.opponent2_from is None
+
+    m1.disconnect(m2)
+    m1.connect_to(m2, LinkType.WINNER, 2)
+    assert m1.winner_to == m2
+    assert m1.loser_to is None
+    assert m2.opponent1_from is None
+    assert m2.opponent2_from == m1
+
+    m1.disconnect(m2)
+    m1.connect_to(m2, LinkType.LOSER, 2)
+    assert m1.winner_to is None
+    assert m1.loser_to == m2
+    assert m2.opponent1_from is None
+    assert m2.opponent2_from == m1
+
+
+def test_connection_raises_error():
+    m1 = Match()
+    m2 = Match()
+    with pytest.raises(BracketError):
+        m1.connect_to(m2, LinkType.WINNER, 3)
+        m1.disconnect(m2)
 
 
 def test_opponent_propagation():
@@ -9,14 +49,10 @@ def test_opponent_propagation():
     wMatch = Match()
     lMatch = Match()
 
-    m1._winnerTo = wMatch
-    wMatch._opponent1From = m1
-    m2._winnerTo = wMatch
-    wMatch._opponent2From = m2
-    m1._loserTo = lMatch
-    lMatch._opponent1From = m1
-    m2._loserTo = lMatch
-    lMatch._opponent2From = m2
+    m1.connect_to(wMatch, LinkType.WINNER, 1)
+    m2.connect_to(wMatch, LinkType.WINNER, 2)
+    m1.connect_to(lMatch, LinkType.LOSER, 1)
+    m2.connect_to(lMatch, LinkType.LOSER, 2)
 
     m1._opponent1 = "Seed 1"
     m1._opponent2 = "Seed 4"
@@ -44,11 +80,11 @@ def test_winning_side_raises_error():
 
 def test_source_match_raises_error():
     m1 = Match()
-    m1._opponent1From = Match()
+    m1._opponent1_from = Match()
 
     m2 = Match()
-    m2._opponent1From = Match()
-    m2._opponent2From = Match()
+    m2._opponent1_from = Match()
+    m2._opponent2_from = Match()
 
     m3 = Match()
 
